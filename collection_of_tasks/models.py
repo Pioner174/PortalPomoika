6ab34575491ed702c5b3e14ee9a9ae2156a34285
Roman <r.matanin@rfsrf.ru>
2021-02-11 17:29:21 +0500

    add models

--- collection_of_tasks/models.py
+++ collection_of_tasks/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+class Organization(models.Model):
+    Guid_id =  models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    Id_higher_organization = models.OneToOneField(Guid_id,)
+    Name = models.
