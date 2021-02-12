e8dae952a084707bbce7480f2e6ef5b5d962fa11
Roman <r.matanin@rfsrf.ru>
2021-02-12 16:11:31 +0500

    Model Organization modified

--- collection_of_tasks/models.py
+++ collection_of_tasks/models.py
@@ -5,5 +5,15 @@ import uuid
 
 class Organization(models.Model):
     Guid_id =  models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
-    Id_higher_organization = models.OneToOneField(Guid_id,)
-    Name = models.
+    Id_higher_organization = models.OneToOneField(Guid_id,blank=True)
+    Name = models.CharField(verbose_name="Название организации", max_length=150)
+    Index = models.IntegerField(verbose_name="Индекс местонахождения", max_length=10)
+    Location = models.CharField(verbose_name="Местонахождение"max_length=150)
+    Postal_code = models.IntegerField(verbose_name="Почтовый индекс", max_length=10)
+    Postal_location = models.CharField(verbose_name="Почтовый адрес"max_length=150)
+    INN = models.IntegerField(verbose_name="ИНН", max_length=10)
+    KPP = models.IntegerField(verbose_name="КПП", max_length=9)
+    OGRN = models.IntegerField(verbose_name="ОГРН", max_length=13, blank=True)
+    OKPO = models.IntegerField(verbose_name="ОКПО", max_length=8, blank=True)
+    
+
